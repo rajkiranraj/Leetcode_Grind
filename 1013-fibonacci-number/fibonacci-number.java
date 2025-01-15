51e93ba88a931815d64861c08408b179ec3ccdd0
RUDRA RAJ KIRAN <rajkiranraj0000@gmail.com>
2025-01-15 23:04:31 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (47.24%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+  int nextTerm = 0;
+    public int fib(int n) {
+      int term1 = 0;
+      int term2 = 1;
+      for(int i = 0;i<n;i++){
+        nextTerm = term1+term2;
+        term1 = term2;
+        term2 = nextTerm;
+      }  
+      return term1;
+    }  
+}
