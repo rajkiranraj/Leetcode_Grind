cfc3ebf7f7c889dbf8095c761f9a7ee5f66601b8
RUDRA RAJ KIRAN <rajkiranraj0000@gmail.com>
2025-01-20 23:20:21 +0530

    Time: 16 ms (17.42%) | Memory: 44.9 MB (33.95%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        //two pointer approach
+         s = s.toLowerCase().replaceAll("[^A-Z0-9a-z]","");
+        int i =0;
+        int j = s.length()-1;
+        while(i<=j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
