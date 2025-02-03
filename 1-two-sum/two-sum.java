784490163825b46462c1a6923d02573b5d697304
RUDRA RAJ KIRAN <rajkiranraj0000@gmail.com>
2025-02-03 23:39:33 +0530

    Update two-sum.java

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -6,6 +6,7 @@ class Solution {
                 if (nums[i] + nums[j] == target) {
                     return new int[]{i, j}; 
                 }
+                
             }
         }
         return new int[]{}; 
