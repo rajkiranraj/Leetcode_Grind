1691401a452ad6448edc1b9632cdbe83dd28745c
RUDRA RAJ KIRAN <rajkiranraj0000@gmail.com>
2025-02-07 23:08:30 +0530

    Update two-sum.java

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
+        
         for (int i = 0; i < n; i++) {
             
             for (int j = i + 1; j < n; j++) {
